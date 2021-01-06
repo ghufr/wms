a2f29d03f8a8834869f782b520d29b83909c559a
Fildzaahns <sitifildzaah@gmail.com>
2021-01-06 17:00:38 +0700

    create n read category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     // TODO: Control CRUD
     // Fildzah
+    public function create(Request $req)
+
+    {
+        Category::create([
+            'name' => $req->name,
+            'description' => $req->description
+        ]);
+        return view('categories');
+    }
+
+    public function list()
+    {
+        $categories = Category::all();
+        return view('categories', ['categories' => $categories]);
+    }
 }
