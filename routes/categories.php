6abb58c3a3f421e661b8f96d43272ddf2ba51466
Fildzaahns <sitifildzaah@gmail.com>
2021-01-09 23:55:24 +0700

    Edit n Delete Category

--- routes/categories.php
+++ routes/categories.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 // Fildzah
-Route::get('/', [CategoryController::class, 'list']
-)->name("categories");
-
+Route::get('/', [CategoryController::class, 'list'])->name("categories");
 Route::get('/input', function () {
 	return view('category_detail');
 })->name("category.input");
+Route::post('/', [CategoryController::class, 'create'])->name('categories.create');
 
-Route::post('/', [CategoryController::class, 'create'])-> name ('categories.create');
+Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::get('/delete/{id}', [CategoryController::class, 'delete'])->name('categories.delete');
+Route::get('/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
